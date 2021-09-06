5dd6ca3b93e1766252e5439e9ac55e74992475f5
Bilgekaan Yilmaz <bilgekaan.yilmaz36@gmail.com>
2021-09-06 23:44:48 +0300

    Quizapp final version

--- script.js
+++ script.js
@@ -42,41 +42,100 @@ let questions = [
 ];
 
 let currentQuestion = 0;
-
+let rightQuestions = 0;
+let AUDIO_SUCCESS = new Audio('sound/start.mp3');
+let AUDIO_FAIL = new Audio('sound/end.mp3');
 
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length;
-
     showQuestion();
 }
 
 function showQuestion() {
-    let question = questions[currentQuestion];
 
+    if (gameIsOver()) {
+        showEndScreen();
+    } else {
+        updateProgressBar();
+        updateToNextQuestion();
+    }
+}
+
+function gameIsOver() {
+    return currentQuestion >= questions.length
+}
+
+function showEndScreen() {
+    document.getElementById('endScreen').style = '';
+    document.getElementById('questionBody').style = 'display: none;';
+    document.getElementById('amount-of-questions').innerHTML = questions.length;
+    document.getElementById('header-image').src = "img/trophy.png";
+}
+
+function updateToNextQuestion() {
+
+    let question = questions[currentQuestion];
+    document.getElementById('current-question').innerHTML = currentQuestion + 1;
     document.getElementById('questiontext').innerHTML = question['question'];
     document.getElementById('answer_1').innerHTML = question['answer_1'];
     document.getElementById('answer_2').innerHTML = question['answer_2'];
     document.getElementById('answer_3').innerHTML = question['answer_3'];
     document.getElementById('answer_4').innerHTML = question['answer_4'];
-
 }
 
 function answer(selection) {
-    let question = questions[currentQuestion];
-    console.log('Selected answer is', selection);
-    let selectedQuestionNumber = selection.slice(-1);
-    console.log('selectedQuestionNumber is', selectedQuestionNumber)
-    console.log('Current question is', question['right_answer']);
+    let question = questions[currentQuestion]; // lade JSON Array auf variable
+    let selectedQuestionNumber = selection.slice(-1); // letzter Buchstabe von selection selektieren
+    let idOfRightAnswer = `answer_${question['right_answer']}`; // Lösung vom Array in Variable laden
 
-    let idOfRightAnswer = `answer_${question['right_answer']}`;
-
-    if (selectedQuestionNumber == question['right_answer']) {
+    if (rightAnswerSelected(selectedQuestionNumber, question)) {
         document.getElementById(selection).parentNode.classList.add('bg-success');
-        console.log('richtige Antwort!!!')
+        document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+        AUDIO_SUCCESS.play();
+        rightQuestions++;
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
-        console.log('falsche Antwort!!!')
+        AUDIO_FAIL.play();
     }
     document.getElementById('next-button').disabled = false;
+}
+
+function rightAnswerSelected(selectedQuestionNumber, question) {
+    return selectedQuestionNumber == question['right_answer']
+}
+
+function nextQuestion() {
+    currentQuestion++; // z.B von 0 auf 1
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons();
+    showQuestion();
+}
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+}
+
+function updateProgressBar() {
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100);
+    console.log('Fortschritt:', percent);
+    document.getElementById('progressBar').style = `width: ${percent}%`;
+    document.getElementById('progressBar').innerHTML = percent + '%';
+}
+
+function restartGame() {
+    document.getElementById('header-image').src = "img/pencil.jpg";
+    document.getElementById('endScreen').style = 'display: none;'; // Endscreen ausblenden
+    document.getElementById('questionBody').style = ''; // questionBody wieder anzeigen
+    currentQuestion = 0;
+    rightQuestions = 0;
+    init();
 }
